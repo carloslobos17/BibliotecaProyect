853c1cad56cbc4564894288f460dbe0cffb36e74
gusrivas <gustavo.rivas1270@gmail.com>
2024-11-15 15:48:10 -0600

    crud de bibliotecarios

---
+++ CapaAccesoDatos/Repositorios/BibliotecarioRepositorios/IBibliotecarioRepositorio.cs
@@ -0,0 +1,12 @@
+﻿using CapaEntidad.Entidades;
+
+namespace CapaAccesoDatos.Repositorios.BibliotecarioRepositorios
+{
+    public interface IBibliotecarioRepositorio
+    {
+        void AgregarBibliotecario(Usuario usuario);
+        void EditarBibliotecario(Usuario usuario);
+        void EliminarBibliotecario(int id);
+        IEnumerable<Usuario> ObtenerBibliotecario();
+    }
+}
---
+++ CapaNegocios/Servicios/BibliotecarioServicios/IBibliotecarioServicio.cs
@@ -0,0 +1,12 @@
+﻿using CapaEntidad.Entidades;
+
+namespace CapaNegocios.Servicios.BibliotecarioServicios
+{
+    public interface IBibliotecarioServicio
+    {
+        void AgregarBibliotecario(Usuario usuario);
+        void EditarBibliotecario(Usuario usuario);
+        void EliminarBibliotecario(int id);
+        IEnumerable<Usuario> GetAllLibros();
+    }
+}
